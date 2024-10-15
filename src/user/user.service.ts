670a6a897651205a0dd02066155305a52ffb7253
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-10-15 03:08:00 +0300

    Enhance all methods in user service to use transactions - Add transaction to create user method - Inject transaction manager in user service - pass session to all methods in user service that create, update or delete data to able use it when needed with default session is null - pass session to all database queries

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,11 +7,12 @@ import { Injectable,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserByAdminDto } from './dto/update-user-by-admin.dto';
-import { Model } from 'mongoose';
+import { ClientSession, Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { PasswordService } from 'src/password/password.service';
 import { User, UserDocument } from './schemas/user.schema';
 import { EmailService } from 'src/common/email.service';
+import { TransactionService } from 'src/transaction/transaction.service';
 
 /* User Service with methods for CRUD operations
     Attributes:
@@ -25,11 +26,12 @@ import { EmailService } from 'src/common/email.service';
 */
 @Injectable()
 export class UserService {
-  // Inject the User model into the service, and the PasswordService and EmailService
+  // Inject the User model, PasswordService, EmailService and TransactionService
   constructor(
     @InjectModel(User.name) private userModel: Model<UserDocument>,
     @Inject(forwardRef(() => PasswordService)) private readonly passwordService: PasswordService,
     private readonly emailService: EmailService,
+    private readonly transactionService: TransactionService,
   ) {}
 
   /* create method to create a new user account
@@ -42,24 +44,26 @@ export class UserService {
       -InternalServerErrorException: If error occurs while saving user
   */
   async create(createUserDto: CreateUserDto): Promise<User> {
-    try {
-      // generate a random password for the user account
-      const password = this.passwordService.generateRandomPassword();
-      const hashPassword = await this.passwordService.hashPassword(password);
-      // create a new user object
-      const userData = { ...createUserDto, password: hashPassword };
-      const newUser = new this.userModel(userData);
-      const createdUser = await newUser.save();
-      // send account credentials email to the user
-      await this.emailService.sendAccountCredentials(createdUser.name, createdUser.email, password);
-      return createdUser;
-    } catch (error) {
-      if (error.code === 11000) {
-        throw new ConflictException('User already exists');
-      } else {
-        throw new InternalServerErrorException('Error while saving user');
+    return this.transactionService.withTransaction(async (session) => {
+      try {
+        // generate a random password for the user account
+        const password = this.passwordService.generateRandomPassword();
+        const hashPassword = await this.passwordService.hashPassword(password);
+        // create a new user object
+        const userData = { ...createUserDto, password: hashPassword };
+        const newUser = new this.userModel(userData);
+        const createdUser = await newUser.save({ session });
+        // send account credentials email to the user
+        await this.emailService.sendAccountCredentials(createdUser.name, createdUser.email, password);
+        return createdUser;
+      } catch (error) {
+        if (error.code === 11000) {
+          throw new ConflictException('User already exists');
+        } else {
+          throw new InternalServerErrorException('An Error occurred while creating the user');
+        }
       }
-    }
+    });
   }
 
   /* Get all users with optional classCategory and return the list of users
@@ -105,13 +109,14 @@ export class UserService {
       Parameters:
         - id: user id
         - updateUserDto: UpdateUserDto object
+        - session: ClientSession (optional)
       Returns:
         - user details in User format
   */
-  async update(id: string, updateUserByAdminDto: UpdateUserByAdminDto) {
+  async update(id: string, updateUserByAdminDto: UpdateUserByAdminDto, session: ClientSession = null) : Promise<User> {
     try {
       // find the user by id and update the user details
-      const updatedUser = await this.userModel.findByIdAndUpdate(id, updateUserByAdminDto, { new: true }).exec();
+      const updatedUser = await this.userModel.findByIdAndUpdate(id, updateUserByAdminDto, { new: true, session }).exec();
       if (!updatedUser) {
         throw new NotFoundException('User not found');
       }
@@ -127,15 +132,16 @@ export class UserService {
   /* Delete user by id
       Parameters:
         - id: user id
+        - session: ClientSession (optional
       Returns:
         - message: string
       Errors:
         - NotFoundException: If user with the id does not exist
         - InternalServerErrorException: An error occurred while Deleting the user
   */
-  async remove(id: string) : Promise<{ message: string }> {
+  async remove(id: string, session: ClientSession = null) : Promise<{ message: string }> {
     try {
-      const user = await this.userModel.findByIdAndDelete(id).exec();
+      const user = await this.userModel.findByIdAndDelete(id, { session }).exec();
       if (!user) {
         throw new NotFoundException('User not found');
       }
@@ -176,15 +182,16 @@ export class UserService {
     Parameters:
       - id: string
       - data: any
+      - session: ClientSession (optional)
     Returns:
       - user: User object
     Errors:
       - NotFoundException: If user with the id not found
       - InternalServerErrorException: An error occurred while Updating the user details
   */
-  async updateWithoutDtoById(id: string, data: Object) : Promise<UserDocument> {
+  async updateWithoutDtoById(id: string, data: Object, session: ClientSession = null) : Promise<UserDocument> {
     try {
-      const user = await this.userModel.findByIdAndUpdate(id, data, { new: true }).exec();
+      const user = await this.userModel.findByIdAndUpdate(id, data, { new: true, session }).exec();
       if (!user) {
         throw new NotFoundException('User not found');
       }
@@ -201,15 +208,16 @@ export class UserService {
     Parameters:
       - fields: object
       - data: any
+      - session: ClientSession (optional)
     Returns:
       - user: User object
     Errors:
       - NotFoundException: If user with the fields not found
       - InternalServerErrorException: An error occurred while Updating the user details
   */
-  async updateWithoutDtoByFields(fields: object, data: Object) : Promise<UserDocument> {
+  async updateWithoutDtoByFields(fields: object, data: Object, session: ClientSession = null) : Promise<UserDocument> {
     try {
-      const user = await this.userModel.findOneAndUpdate(fields, data, { new: true }).exec();
+      const user = await this.userModel.findOneAndUpdate(fields, data, { new: true, session }).exec();
       if (!user) {
         throw new NotFoundException('User not found');
       }
@@ -225,15 +233,17 @@ export class UserService {
   /* deactivateExpiredUsers method to deactivate users whose activation has expired
     Parameters:
       - minAgo: Date object
+      - session: ClientSession (optional)
     Returns:
       - message: string
   */
-  async deactivateExpiredUsers(dateAgo: Date) : Promise<{ message: string }> {
+  async deactivateExpiredUsers(dateAgo: Date, session: ClientSession = null) : Promise<{ message: string }> {
     try {
       // find users whose activation has expired and deactivate them
       const users = await this.userModel.updateMany(
         { isActive: true, lastActivatedDate: { $lte: dateAgo } },
-        { isActive: false }
+        { isActive: false },
+        { session }
       ).exec();
       // return message of number of users deactivated
       return { message: `Number of users deactivated: ${users.modifiedCount}` };
